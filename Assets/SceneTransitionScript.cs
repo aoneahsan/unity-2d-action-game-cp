3b9566955371f499db86feb6accaa963ffb4b5e5
Ahsan Mahmood <ahsan@perkforce.com>
2021-08-01 20:43:23 +0500

    fix: updated

---
+++ Assets/SceneTransitionScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitionScript : MonoBehaviour
+{
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        StartCoroutine(SceneTransition(sceneName));
+    }
+
+    IEnumerator SceneTransition(string sceneName)
+    {
+        animator.SetTrigger("end");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(sceneName);
+    }
+}
--- Assets/Scripts/Enemies/Boss/BossScript.cs
+++ Assets/Scripts/Enemies/Boss/BossScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BossScript : MonoBehaviour
 {
@@ -24,6 +25,7 @@ public class BossScript : MonoBehaviour
 
     private Animator animator;
 
+    private GameObject bossHealthBar;
     private Slider bossHealthBarSlider;
 
     // Start is called before the first frame update
